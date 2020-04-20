97a5d86daa23df781cecd27668c31340f4b00a21
Tom Perrine <tom.perrine@gmail.com>
2020-04-20 14:17:48 -0700

    Main-ify and clean up output, so flushed etc.

--- fping6.py
+++ fping6.py
@@ -4,15 +4,22 @@ import fileinput
 import subprocess
 import os
 
-# input is a file of hostnames or IPv6 addresses
-with open(os.devnull, "wb") as limbo:
-    for line in fileinput.input():
-        line= line.rstrip()
-        result=subprocess.run(["/sbin/ping6", "-c", "1", "-n", line],capture_output=True)
-        if (result.returncode == 0):
-            print (line, "active")
-        else:
-            print (line, "inactive")
 
+
+def main():
+    """take a file of hostnames and IPv6 addresses..."""
+    with open(os.devnull, "wb") as limbo:
+        for line in fileinput.input():
+            line= line.rstrip()
+            print(line, flush=True, end=" ")
+            result=subprocess.run(["/sbin/ping6", "-c", "1", "-n", line],capture_output=True)
+            if (result.returncode == 0):
+                print ("active")
+            else:
+                print ("inactive")
+
+
+if __name__ == '__main__' :
+    main()
 
 

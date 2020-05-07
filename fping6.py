d84442c5159b5c1abf48cb397d66a1d21f44ed87
Tom Perrine <tom.perrine@gmail.com>
2020-05-07 07:49:56 -0700

    fixed to allow comments in target host file

--- fping6.py
+++ fping6.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
-
 import fileinput
 import subprocess
 import os
 
-
-
 def main():
-    """take a file of hostnames and IPv6 addresses..."""
+    """Attempt IPv6 ping to every host listed in the input file.
+    The input file is specified as the only command line argument"""
     with open(os.devnull, "wb") as limbo:
         for line in fileinput.input():
             line= line.rstrip()
+            # allow comments
+            if line.startswith('#'):
+                continue
             print(line, flush=True, end=" ")
             result=subprocess.run(["/sbin/ping6", "-c", "1", "-n", line],capture_output=True)
             if (result.returncode == 0):
